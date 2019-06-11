34f3cd669aa152ce402b80539bf565a2e7a5312a
HuangHao <419493745@qq.com>
2019-06-11 15:42:54 +0800

    the second modification

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! 1234567");
+        System.out.println("Hello World! 123456789");
     }
 }
